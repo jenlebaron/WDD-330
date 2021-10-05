7c4be0093e6113f275faff8b9edb7b1e34c1d938
jenlebaron <leb20008@byui.edu>
2021-10-05 13:50:04 -0700

    added notes and team project

---
+++ Week 4/w04_team.js
@@ -0,0 +1,15 @@
+let play_board = ["", "", "", "", "", "", "", "", ""];
+
+const board_container = document.querySelector(".play-area");
+
+const render_board = () => {
+    board_container.innerHTML = "";
+    play_board.forEach((e,i) => {
+        board_container.innerHTML += '<div id="block_${i}" class="block" onclick="addPlayerMove(${i})">${play_board[i]}</div>';
+        if (e == player || e == computer) {
+            document.querySelector('#block_${i}').classList.add("occupied");
+        }
+    });
+};
+
+render_board();
