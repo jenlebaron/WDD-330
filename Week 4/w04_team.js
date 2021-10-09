18a1e6f3270308be682df04a936f218220e4a095
jenlebaron <leb20008@byui.edu>
2021-10-09 10:33:59 -0700

    Hopefuly this will work

--- Week 4/w04_team.js
+++ Week 4/w04_team.js
@@ -11,27 +11,27 @@ const board = document.querySelector('.board');
         else player = player1;
       }
       // table version
-    //   function resetBoard() {
-    //     console.dir(board);
-    //     for (let i = 0; i < board.rows.length; i++) {
-    //       let row = board.rows[i];
-    //       for (let j = 0; j < row.cells.length; j++) {
-    //         row.cells[j].innerHTML = '';
-    //       }
-    //     }
-    //   }
-      // div version
-      function resetBoardDiv() {
-        const divBoard = document.querySelector('.divBoard');
-        for (let i = 0; i < divBoard.children.length; i++) {
-          divBoard.children[i].innerText = '';
+      function resetBoard() {
+        console.dir(board);
+        for (let i = 0; i < board.rows.length; i++) {
+          let row = board.rows[i];
+          for (let j = 0; j < row.cells.length; j++) {
+            row.cells[j].innerHTML = '';
+          }
         }
-        const children = Array.from(divBoard.children);
-        const empty = children.filter(function(child) {
-          return child.innerText == 'X' || child.innerText == 'O';
-        });
-        console.log(empty);
       }
+      // div version
+    //   function resetBoardDiv() {
+    //     const divBoard = document.querySelector('.divBoard');
+    //     for (let i = 0; i < divBoard.children.length; i++) {
+    //       divBoard.children[i].innerText = '';
+    //     }
+    //     const children = Array.from(divBoard.children);
+    //     const empty = children.filter(function(child) {
+    //       return child.innerText == 'X' || child.innerText == 'O';
+    //     });
+    //     console.log(empty);
+    //   }
       board.addEventListener('click', addPiece);
       divBoard.addEventListener('click', addPiece);
       reset.addEventListener('click', resetBoardDiv);
