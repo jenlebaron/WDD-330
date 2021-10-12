d1bb12cca8f14f65297d8bd0d26e2bd4577344bf
jenlebaron <leb20008@byui.edu>
2021-10-12 12:00:06 -0700

    Added notes for week 5 and group project

---
+++ Week 5/hikes.js
@@ -0,0 +1,10 @@
+import Hikes from "./hiking-start.js";
+//on load grab the array and insert it into the page
+const myHikes = new Hikes("hikes");
+window.addEventListener("load", () => {
+  myHikes.showHikeList();
+});
+myHikes.hikeList;
+  
+
+  
