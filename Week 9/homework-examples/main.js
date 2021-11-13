fa26000d538c1d5750a2f9ce44a8c8287543a190
jenlebaron <leb20008@byui.edu>
2021-11-13 11:35:19 -0800

    added more css to homework example and drumset

--- Week 9/homework-examples/main.js
+++ Week 9/homework-examples/main.js
@@ -1,18 +1,20 @@
-// const squareElement = document.getElementById('square');
-// let angle = 0;
-
-// setInterval( () => {
-//     angle = (angle + 2) % 360;
-//     squareElement.style.transform = `rotate(${angle}deg)`
-// }, 1000/60);
-
 const squareElement = document.getElementById('square');
 let angle = 0;
-
 function rotate() {
     angle = (angle + 2)%360;
     squareElement.style.transform = `rotate(${angle}deg)`
     window.requestAnimationFrame(rotate);
 }
+console.log('square');
+
+const circleElement = document.getElementById('circle');
+let angle2 = 0;
+setInterval( () =>{
+    angle2 = (angle2 + 2)%360;
+    circleElement.style.transform = `rotate(${angle2}deg)`
+}, 1000/60);
+
+console.log('circle');
 
 const id = requestAnimationFrame(rotate);
+
